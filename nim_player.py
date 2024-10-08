cef40571db02224a7575fdf0179088d4d8acb5b7
Leif Forrest <92558165+thekinghobbit@users.noreply.github.com>
2024-10-07 21:41:49 -0700

    test pass

--- nim_player.py
+++ nim_player.py
@@ -1,6 +1,11 @@
-class NimPlayer():
-    def __init__(self, name):
-        self.name = name
-
-    def play(self,board):
+class NimPlayer:
+    def __init__(self):
         pass
+
+    def play(self, board):
+        # Find the first non-empty row and take one stick from it
+        for i in range(len(board)):
+            if board[i] > 0:
+                board[i] -= 1
+                break
+        return board
